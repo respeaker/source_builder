112d3856c75e6c925a7a5ec41dff81a3e9a3fc75
Baozhu Zuo <zuobaozhu@gmail.com>
2017-08-05 15:31:57 +0800

    u-boot: rewrite the mk-uboot.sh

--- mk-uboot.sh
+++ mk-uboot.sh
@@ -1,106 +1,28 @@
-#!/bin/bash
+#!/bin/sh -e
+#
+# Copyright (c) 2017 Baozhu Zuo <baozhu.zuo@foxmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
-export ARCH=arm
-export CROSS_COMPILE=arm-linux-gnueabihf-
-
-LOCALPATH=$(pwd)
-OUT=${LOCALPATH}/out
-TOOLPATH=${LOCALPATH}/rkbin/tools
-BOARD=$1
-
-PATH=$PATH:$TOOLPATH
-
-finish() {
-	echo -e "\e[31m MAKE UBOOT IMAGE FAILED.\e[0m"
-	exit -1
-}
-trap finish ERR
-
-if [ $# != 1 ]; then
-	BOARD=rk3288-evb
-fi
-
-[ ! -d ${OUT} ] && mkdir ${OUT}
-[ ! -d ${OUT}/u-boot ] && mkdir ${OUT}/u-boot
-
-source $LOCALPATH/build/board_configs.sh $BOARD
-
-if [ $? -ne 0 ]; then
-	exit
-fi
 
 echo -e "\e[36m Building U-boot for ${BOARD} board! \e[0m"
 echo -e "\e[36m Using ${UBOOT_DEFCONFIG} \e[0m"
 
-cd ${LOCALPATH}/u-boot
-make ${UBOOT_DEFCONFIG} all
-
-if [ "${CHIP}" == "rk3288" ] || [ "${CHIP}" == "rk322x" ] || [ "${CHIP}" == "rk3036" ]; then
-	tools/mkimage -n ${CHIP} -T \
-		rksd -d spl/u-boot-spl-dtb.bin idbloader.img
-	cat u-boot-dtb.bin >>idbloader.img
-	cp idbloader.img ${OUT}/u-boot/
-elif [ "${CHIP}" == "rk3328" ]; then
-	$TOOLPATH/loaderimage --pack --uboot ./u-boot-dtb.bin uboot.img
-
-	dd if=../rkbin/rk33/rk3328_ddr_786MHz_v1.06.bin of=DDRTEMP bs=4 skip=1
-	tools/mkimage -n rk3328 -T rksd -d DDRTEMP idbloader.img
-	cat ../rkbin/rk33/rk3328_miniloader_v2.43.bin >> idbloader.img
-	cp idbloader.img ${OUT}/u-boot/	
-	cp ../rkbin/rk33/rk3328_loader_ddr786_v1.06.243.bin ${OUT}/u-boot/
-
-	cat >trust.ini <<EOF
-[VERSION]
-MAJOR=1
-MINOR=2
-[BL30_OPTION]
-SEC=0
-[BL31_OPTION]
-SEC=1
-PATH=../rkbin/rk33/rk3328_bl31_v1.34.bin
-ADDR=0x10000
-[BL32_OPTION]
-SEC=0
-[BL33_OPTION]
-SEC=0
-[OUTPUT]
-PATH=trust.img
-EOF
-
-	$TOOLPATH/trust_merger trust.ini
-
-	cp uboot.img ${OUT}/u-boot/
-	mv trust.img ${OUT}/u-boot/
-elif [ "${CHIP}" == "rk3399" ]; then
-	$TOOLPATH/loaderimage --pack --uboot ./u-boot-dtb.bin uboot.img
-
-	tools/mkimage -n rk3399 -T rksd -d ../rkbin/rk33/rk3399_ddr_800MHz_v1.08.bin idbloader.img
-	cat ../rkbin/rk33/rk3399_miniloader_v1.06.bin >> idbloader.img
-	cp idbloader.img ${OUT}/u-boot/
-	cp ../rkbin/rk33/rk3399_loader_v1.08.106.bin ${OUT}/u-boot/
-
-	cat >trust.ini <<EOF
-[VERSION]
-MAJOR=1
-MINOR=0
-[BL30_OPTION]
-SEC=0
-[BL31_OPTION]
-SEC=1
-PATH=../rkbin/rk33/rk3399_bl31_v1.00.elf
-ADDR=0x10000
-[BL32_OPTION]
-SEC=0
-[BL33_OPTION]
-SEC=0
-[OUTPUT]
-PATH=trust.img
-EOF
-
-	$TOOLPATH/trust_merger trust.ini
-
-	cp uboot.img ${OUT}/u-boot/
-	mv trust.img ${OUT}/u-boot/
-fi
 
 echo -e "\e[36m U-boot IMAGE READY! \e[0m"
